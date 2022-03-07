3df891af9f44eeec1a1eb1dfe19388dc2767734c
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-03-08 01:59:17 +0600

    fifth commit

--- app.js
+++ app.js
@@ -16,9 +16,24 @@ const showArtist = ({artists}) =>{
   const artistContainer = elementById("artists")
   artists.forEach(artist => {
     const div = document.createElement('div');
-    div.innerHTML =  `
-        <p>${artist.strArtist}</p>
-    `
+    div.classList.add("artist-card");
+    div.innerHTML =  `<div class="image-container">
+    <div class="image-container-inner">
+      <img
+        src="${artist.strArtistThumb}"
+        alt=""
+      />
+    </div>
+  </div>
+  <div class="info-container">
+    <h1>${artist.strArtist}</h1>
+    <p>Country: ${artist.strCountry}</p>
+    <p>Style: ${artist.strGenre}</p>
+  </div>
+  <button class="album-button">
+    <i class="fa-solid fa-compact-disc"></i>
+    <p onclick="fetchAlbums('${artist.idArtist}')" class="button-title">Albums</p>
+  </button>`;
     artistContainer.appendChild(div);
   });
   
