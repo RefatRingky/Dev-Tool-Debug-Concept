ec6206602c8c8b05697bf0fbd591f08a62d8ddb9
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-03-08 01:27:34 +0600

    app commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const elementById = (id) =>{
+    // console.log(id);
+   return document.getElementById(id);
+};
+
+
+const handleSearch = () =>{
+ const keyword = elementById("keyword");
+ const url = `https://theaudiodb.com/api/v1/json/2/search.php?s=${keyword.value}`;
+  console.log(url);
+  fetch(url)
+  .then((res)=> res.json())
+  .then((data) => showArtist(data));
+};
+const showArtist = (data) =>{
+  console.log(data);
+};
